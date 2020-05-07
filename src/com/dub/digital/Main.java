989d8b945607a3b27fb275844fa915c0954a8167
student <student@CNN-Spare.local>
2020-05-06 23:09:15 -0400

    Condition statement in main java file

--- src/com/dub/digital/Main.java
+++ src/com/dub/digital/Main.java
@@ -3,6 +3,14 @@ package com.dub.digital;
 public class Main {
 
     public static void main(String[] args) {
+      int userCount = 0;
+      int cpuCount = 0;
+      int showDownCount = 0;
+
 	  Game game = new Game();
+
+	  for(i = 0; i <=10; i++){
+	      
+      }
     }
 }
