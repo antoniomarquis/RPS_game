07b1985d1e615d6d1e15922b8d91c7c435405332
student <student@CNN-Spare.local>
2020-05-06 23:27:10 -0400

    startGame used in Main class. I tried to write another prior to using the startGame method that was already created.

--- src/com/dub/digital/Main.java
+++ src/com/dub/digital/Main.java
@@ -9,8 +9,21 @@ public class Main {
 
 	  Game game = new Game();
 
-	  for(i = 0; i <=10; i++){
-	      
+	  for(i = 0; i < 10; i++){
+         int faceOff = game.startGame();
+         if(faceOff == 1){
+             userCount = userCount + 1;  //could not get userCount++ to work
+         } else {
+             cpuCount = cpuCount + 1;
+         }
+         showDownCount++;
+         if (showDownCount == 10){
+             if(cpuCount > userCount) {
+                 System.out.println("CPU is the winner!!! CPU CUT UP!!");
+             }else{
+                 System.out.println("USER is the winner!!! U ROCK!");
+             }
+         }
       }
     }
 }
